880775296bca4162065b455ad630df275bef1cae
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-05-07 01:00:57 -0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,19 +1,11 @@
+<?php  
+    session_start();
+    require 'dbconfi/confi.php';
+?>
+
 <?php
     require "header.php";
-    $servername = "remotemysql.com";
-    $username = "kfm2yvoF5R";
-    $password = "4vkzHfeBh6";
-    $dbname = "kfm2yvoF5R";
-
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password);
-    // Check connection
-    if (!$conn) {
-        echo("Connection failed ");
-    }
-    if(!mysqli_select_db($conn,$dbname)){
-        echo "Database not selected";
-    }
+    
     $password = $_POST["password"];
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
@@ -29,9 +21,18 @@
 
     $sql = "INSERT INTO civilian_detail (password, first_name, last_name, gender, NIC_num, address, district,village,street, Occupation, phone_num, email)
     VALUES ('$password', '$first_name', '$last_name', '$gender', '$nic', '$address', '$district', '$village','$street','$occupation', '$phone_number', '$email_address')";
-    $query_run= mysqli_query($conn,$sql);
+    $query_run= mysqli_query($con,$sql);
 
     if($query_run ){
+        
+        $query="ALTER TABLE disaster_events ADD COLUMN `user_".$nic."` varchar(50) NOT NULL DEFAULT 'not_set not_requested not_applied'";
+        $query_run= mysqli_query($con,$query);
+
+        $query1 = "CREATE TABLE `user_".$nic."` (Notification_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,Date date, Time time,Content text)"; 
+        $query1_run= mysqli_query($notification_DB,$query1);
+
+       
+
         echo '<script type="text/javascript"> alert ("Data Uploaded") </script>';
          header('location:member.php');
     }else{
